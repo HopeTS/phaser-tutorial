058199e090226ae6570652dac62be0a7ecff71e8
Robby Bergers <bergersr97@gmail.com>
2022-04-22 23:28:57 -0400

    fix pipe spawning

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -27,16 +27,18 @@ function preload() {
   return;
 }
 
-let bottomPipe1 = null;
-let topPipe1 = null;
-
-let bottomPipe2 = null;
-let topPipe2 = null;
-
-let pipeHole = 0;
-
-let bird = null;
+// General game data
 let totalDelta = null;
+
+// Pipe config data
+const PIPE_VERTICAL_DISTANCE_RANGE = [100, 200];
+const PIPE_HORIZONTAL_DISTANCE = 400;
+const PIPES_TO_RENDER = 4;
+let bottomPipe = null;
+let topPipe = null;
+
+// Bird config data
+let bird = null;
 const VELOCITY = -225;
 
 function create() {
@@ -49,14 +51,47 @@ function create() {
     .setOrigin(0);
   bird.body.gravity.y = 400;
 
-  // Set pipes
-  const initialPipeHole = createPipeHole();
+  // Set pipes (tutorial way - single pipe set)
+  /*   bottomPipe = this.physics.add
+    .sprite(400, pipeVerticalPosition, "pipe")
+    .setOrigin(0, 0);
+  topPipe = this.physics.add
+    .sprite(400, bottomPipe.y - pipeVerticalDistance, "pipe")
+    .setOrigin(0, 1); */
+
+  // Set pipes (multiple)
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    console.log("creating new pipe set");
+    let pipeVerticalDistance = Phaser.Math.Between(
+      ...PIPE_VERTICAL_DISTANCE_RANGE
+    );
+    let pipeVerticalPosition = Phaser.Math.Between(
+      0 + 50,
+      config.height - 50 - pipeVerticalDistance
+    );
+    bottomPipe = this.physics.add
+      .sprite(i * PIPE_HORIZONTAL_DISTANCE + 400, pipeVerticalPosition, "pipe")
+      .setOrigin(0, 0);
+    topPipe = this.physics.add
+      .sprite(
+        i * PIPE_HORIZONTAL_DISTANCE + 400,
+        bottomPipe.y - pipeVerticalDistance,
+        "pipe"
+      )
+      .setOrigin(0, 1);
+
+    topPipe.body.velocity.x = -200;
+    bottomPipe.body.velocity.x = -200;
+  }
+
+  // Set pipes ( my way)
+  /* const initialPipeHole = createPipeHole();
   bottomPipe1 = this.physics.add
     .sprite(config.width * 0.3, initialPipeHole - 50, "pipe")
     .setOrigin(0, 1);
   topPipe1 = this.physics.add
     .sprite(config.width * 0.3, initialPipeHole + 50, "pipe")
-    .setOrigin(0, 0);
+    .setOrigin(0, 0); */
 
   // Set controls
   this.input.keyboard.on("keydown_SPACE", flap);
