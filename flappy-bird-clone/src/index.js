22dbada70745942528ffde9632852ccff2cb473d
Robby Bergers <bergersr97@gmail.com>
2022-04-22 21:19:13 -0400

    load background and sprite, add gravity

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -1,4 +1,3 @@
-
 import Phaser from "phaser";
 
 const config = {
@@ -6,23 +5,44 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      gravity: {
+        y: 200,
+      },
+    },
   },
   scene: {
-    preload: preload,
-    create: create
-  }
+    preload,
+    create,
+    update,
+  },
 };
 
+function preload() {
+  // Load images
+  this.load.image("sky", "assets/sky.png");
+
+  // Load sprites
+  this.load.image("bird", "assets/bird.png");
+  return;
+}
+
+let bird = null;
+let totalDelta = null;
+
+function create() {
+  this.add.image(0, 0, "sky").setOrigin(0);
+  bird = this.physics.add
+    .sprite(config.width * 0.1, config.height / 2, "bird")
+    .setOrigin(0);
+
+  return;
+}
+
+function update(time, delta) {
+  totalDelta += delta;
+  return;
+}
+
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
