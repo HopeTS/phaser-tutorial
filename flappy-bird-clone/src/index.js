7e13378acf432f48df3cdb054999012438791176
Robby Bergers <bergersr97@gmail.com>
2022-04-22 21:41:29 -0400

    add bird flapping

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -29,14 +29,15 @@ function preload() {
 
 let bird = null;
 let totalDelta = null;
+const VELOCITY = -200;
 
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0);
   bird = this.physics.add
     .sprite(config.width * 0.1, config.height / 2, "bird")
     .setOrigin(0);
-  bird.body.gravity.y = -200;
-  bird.body.velocity.x = 200;
+
+  this.input.keyboard.on("keydown_SPACE", flap);
   return;
 }
 
@@ -44,12 +45,17 @@ function update(time, delta) {
   totalDelta += delta;
 
   // Move bird back and forth
-  if (bird.body.position.x > 800) {
+  if (bird.body.position.x > config.width - bird.width) {
     bird.body.velocity.x = -200;
   } else if (bird.body.position.x < 0) {
     bird.body.velocity.x = 200;
   }
 
+  return;
+}
+
+function flap() {
+  bird.body.velocity.y = VELOCITY;
   return;
 }
 
